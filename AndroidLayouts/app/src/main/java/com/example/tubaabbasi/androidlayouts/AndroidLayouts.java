eca7f7fafbaeb89258e01f7fde75ac6cb5fe332c
Tuba Abbasi <tsabbasi@gmail.com>
2016-09-11 13:37:56 -0400

    initial commit

---
+++ AndroidLayouts/app/src/main/java/com/example/tubaabbasi/androidlayouts/AndroidLayouts.java
@@ -0,0 +1,13 @@
+package com.example.tubaabbasi.androidlayouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidLayouts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_layouts);
+    }
+}
