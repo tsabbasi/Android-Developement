6e0d897b534bb9c083051276c925f36466aa77e3
Tuba Abbasi <tsabbasi@gmail.com>
2016-09-30 10:15:49 -0400

    Additional Projects

---
+++ TealeamanClassQuiz/app/src/main/java/com/example/tubaabbasi/tealeamanclassquiz/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.tubaabbasi.tealeamanclassquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
+    }
+}
