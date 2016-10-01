a1e79c2c825059f9fc848a6f8ca20edab831fd48
Tuba Abbasi <tsabbasi@gmail.com>
2016-09-30 23:02:04 -0400

    Styling implemented

--- TealeamanClassQuiz/app/src/main/java/com/example/tubaabbasi/tealeamanclassquiz/MainActivity.java
+++ TealeamanClassQuiz/app/src/main/java/com/example/tubaabbasi/tealeamanclassquiz/MainActivity.java
@@ -10,10 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
-
-        setContentView(R.layout.activity_main);
-
-        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
+//        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+//
+//        setContentView(R.layout.activity_main);
+//
+//        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
     }
 }
