d8624a8570c8589c33cf6039e961f51ecc48910f
Tuba Abbasi <tsabbasi@gmail.com>
2016-10-17 09:43:39 -0400

    notebook app continued

--- Notebook/app/src/main/java/com/example/tubaabbasi/notebook/MainActivityListFragment.java
+++ Notebook/app/src/main/java/com/example/tubaabbasi/notebook/MainActivityListFragment.java
@@ -5,6 +5,7 @@ import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
@@ -16,6 +17,14 @@ public class MainActivityListFragment extends ListFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        String[] values = new String[] {"Android", "iPhone", "WindowsMobile", "Blackberry",
+                "WebOS", "Ubuntu", "Windows7", "Mac OS X", "Linux", "OS/2"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, values);
+
+        setListAdapter(adapter);
 
 
 
