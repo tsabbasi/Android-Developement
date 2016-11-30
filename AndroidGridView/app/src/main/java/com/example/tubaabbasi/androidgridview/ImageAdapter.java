3f7be028f0f41ec84b644adfc673b4650d70d72a
Tuba Abbasi <tsabbasi@gmail.com>
2016-11-30 09:44:16 -0500

    Implemented Android Grid View

---
+++ AndroidGridView/app/src/main/java/com/example/tubaabbasi/androidgridview/ImageAdapter.java
@@ -0,0 +1,69 @@
+package com.example.tubaabbasi.androidgridview;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by tubaabbasi on 11/30/16.
+ */
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+    // Constructor
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    @Override
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        }
+        else
+        {
+            imageView = (ImageView) convertView;
+        }
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // Keep all Images in array
+    public Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+}
